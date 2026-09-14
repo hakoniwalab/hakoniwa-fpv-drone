53108a0dcde2e8bf5d71e3a7e1f56787633e9e3b
Takashi Mori <kanetugu2015@gmail.com>
2026-09-15 06:46:40 +0900

    test: lock Master3X evidence-constrained geometry

--- tests/test_master3x_rigid_body_model.py
+++ tests/test_master3x_rigid_body_model.py
@@ -57,6 +57,56 @@ class Master3XRigidBodyModelTest(unittest.TestCase):
             )
         self.assertEqual("hakoniwa.generic.camera-mount", port_interfaces["camera_mount"])
 
+    def test_evidence_constrains_vertical_stack(self):
+        catalogs = load_catalogs(CATALOGS)
+        frame = catalogs.frames.get("speedybee_master3x")
+        primitives = {primitive.name: primitive for primitive in frame.geometry.visual}
+
+        middle = primitives["middle_plate"]
+        top = primitives["top_plate"]
+        self.assertEqual((0.086, 0.034, 0.002), middle.dimensions_m)
+        self.assertEqual((0.091, 0.030, 0.002), top.dimensions_m)
+
+        middle_top = middle.center_m[2] + middle.dimensions_m[2] / 2.0
+        top_bottom = top.center_m[2] - top.dimensions_m[2] / 2.0
+        self.assertAlmostEqual(0.011, top_bottom - middle_top, places=9)
+
+        for name in (
+            "standoff_rear_right",
+            "standoff_rear_left",
+            "standoff_front_right",
+            "standoff_front_left",
+        ):
+            self.assertAlmostEqual(0.011, primitives[name].length_m, places=9)
+
+    def test_front_support_opening_uses_camera_compatibility_width(self):
+        catalogs = load_catalogs(CATALOGS)
+        frame = catalogs.frames.get("speedybee_master3x")
+        primitives = {primitive.name: primitive for primitive in frame.geometry.visual}
+        right = primitives["front_support_right"]
+        left = primitives["front_support_left"]
+
+        right_inner_face = right.center_m[1] + right.dimensions_m[1] / 2.0
+        left_inner_face = left.center_m[1] - left.dimensions_m[1] / 2.0
+        self.assertAlmostEqual(0.020, left_inner_face - right_inner_face, places=9)
+
+    def test_mount_planes_follow_evidence_backed_plates(self):
+        catalogs = load_catalogs(CATALOGS)
+        frame = catalogs.frames.get("speedybee_master3x")
+        primitives = {primitive.name: primitive for primitive in frame.geometry.visual}
+        ports = {port.id: port for port in frame.assembly_ports}
+
+        middle = primitives["middle_plate"]
+        middle_top = middle.center_m[2] + middle.dimensions_m[2] / 2.0
+        self.assertAlmostEqual(middle_top, ports["electronics_mount"].position_m[2], places=9)
+
+        top = primitives["top_plate"]
+        pad = primitives["battery_pad_front"]
+        top_surface = top.center_m[2] + top.dimensions_m[2] / 2.0
+        self.assertGreaterEqual(pad.center_m[2] - pad.dimensions_m[2] / 2.0, top_surface)
+        pad_top = pad.center_m[2] + pad.dimensions_m[2] / 2.0
+        self.assertAlmostEqual(pad_top, ports["battery_mount"].position_m[2], places=9)
+
 
 if __name__ == "__main__":
     unittest.main()
